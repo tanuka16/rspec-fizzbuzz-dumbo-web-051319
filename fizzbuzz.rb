8c34edbba9155c98a83c91b135e8afc3af572884
Tanuka Das <das.tanuka@yahoo.com>
2019-04-21 23:34:39 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,17 +2,26 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
+=begin  if int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
   elsif int % 5 ==0
     "Buzz"
   elsif int % 3 == 0 && int % 5 == 0
     return "FizzBuzz"
   else
-    puts 
-
-
+    puts
   end
+=end
+  if int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
+  elsif int % 3 == 0
+    "Fizz"
+  elsif int % 5 ==0
+    "Buzz"
+  else
+    puts
+  end
+
 
 
 end
